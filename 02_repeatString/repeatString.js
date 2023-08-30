e9f24bc8db222c6de6c5fa0d172c9ff30578b6a1
CloudyKrypton <janetfu04@gmail.com>
2023-08-30 17:31:58 -0400

    Complete 1-7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    grandString = '';
+    if (num < 0) {
+        return "ERROR";
+    } else{
+        for (i = 0; i < num; i++) {
+            grandString += string;
+        }
+        return grandString;
+    }
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, finish) {
+    sum = 0;
+    if (typeof(start) != "number" || typeof(finish) != "number" || start < 0 || finish < 0) {
+        return ("ERROR");
+    } else if (start <= finish) {
+        for (i = start; i <= finish; i++) {
+            sum += i;
+        }
+    } else {
+        for (i = start; i >= finish; i--) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
